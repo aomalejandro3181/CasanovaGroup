6190f39c8a593980d3c826c06c8a7764ef086959
winderson <winderson705@gmail.com>
2010-05-06 00:16:45 -0430

    estilos ssas

--- wp-content/themes/casanova/html/contacto.php
+++ wp-content/themes/casanova/html/contacto.php
@@ -1,33 +1,33 @@
 <div id="contacto" class="container-fluid">
 	<div class="row justify-content-center">
-		<div class="col-xs-12 col-sm-10 col-md-9 col-lg-9">
-			<p id="titulo-c">Contacto</p>
-			<input id="correo" class="border border-secondary rounded-pill" type="email" name="Correo" placeholder="Correo"></div>
+		<div class="col-xs-12 col-sm-11 col-md-9 col-lg-10 col-xl-10">
+			<p id="titulo-c">CONTACTO</p>
+			<input id="correo" class=" col-9 border border-secondary rounded-pill" type="email" name="Correo" placeholder="Correo"></div>
 	</div>
 	<div class="row justify-content-center">
-		<div class="col-xs-12 col-sm-10 col-md-9 col-lg-9">
+		<div class="col-12 col-xs-12 col-sm-11 col-md-9 col-lg-10 col-xl-10">
 		<input id="nombre" class=" border border-secondary rounded-pill" type="text" name="nombre" placeholder="Nombre">
-		<input id="apellido" class=" border border-secondary rounded-pill" type="text" name="apellido" placeholder="apellido">    
+		<input id="apellido" class=" border border-secondary rounded-pill" type="text" name="apellido" placeholder="Apellido">    
 	</div>
 	</div>
 	<div class="row justify-content-center">
-	    <div class="col-xs-12 col-sm-10 col-md-9 col-lg-9">
-	    	<input id="empresa" class="border border-secondary rounded-pill" type="text" name="Empresa" placeholder="empresa">
-	        <input id="telefono" class="border border-secondary rounded-pill" type="number" name="telefono" placeholder="telefono">
+	    <div class="col-12 col-xs-12 col-sm-11 col-md-9 col-lg-10">
+	    	<input id="empresa" class=" border border-secondary rounded-pill" type="text" name="Empresa" placeholder="Empresa">
+	        <input id="telefono" class="border border-secondary rounded-pill" type="number" name="telefono" placeholder="Telefono">
 	    	
 	    </div>
 	</div>
 	<div class="row justify-content-center">
-	   <div class="col-xs-12 col-sm-10 col-md-9 col-lg-9">
-	   	<input id="asunto" class="border border-secondary rounded-pill" type="text" name="asunto" placeholder="asunto">
+	   <div class="col-xs-12 col-sm-11 col-md-9 col-lg-10">
+	   	<input id="asunto" class="border border-secondary rounded-pill" type="text" name="asunto" placeholder="Asunto">
 	   </div>
 	</div>
 	<div class="row justify-content-center">
-	   <div class="col-xs-12 col-sm-10 col-md-9 col-lg-9">
+	   <div class="col-xs-12 col-sm-11 col-md-9 col-lg-10">
 	   	<input id="mensaje" class="border border-secondary " type="mensaje" name="mensaje" placeholder="Mensaje">
 	   </div>
 	</div>
 	<div class="row justify-content-center">
-	   <div class="col-xs-12 col-sm-10 col-md-9 col-lg-9"><input id="enviar" class=" rounded-pill" type="submit" name="enviar" value="Enviar"></div>
+	   <div class="col-xs-12 col-sm-11 col-md-9 col-lg-10"><input id="enviar" class=" rounded-pill" type="submit" name="enviar" value="Enviar"></div>
 	</div>
 </div>
--- wp-content/themes/casanova/html/equipo.php
+++ wp-content/themes/casanova/html/equipo.php
@@ -1,10 +1,11 @@
 <section id="equipo"class="container-fluid">
+	 
 	<div class="row justify-content-center ">
-	  <article class="col-xs-12 col-sm-5 col-md-9 col-lg-9">
-	  	<h2 class="estilo-h2">EQUIPO</h2></BR></BR>
+	  <article class="col-xs-12 col-sm-11 col-md-9 col-lg-10">
+	  	<h2 class="estilo-h2">EQUIPO</h2>
 	   </article>
 	      <article class="w-100"></article>
-	    <article class=" col-xs-12 col-sm-5 col-md-9 col-lg-9">
+	    <article class=" col-xs-12 col-sm-11 col-md-9 col-lg-10">
 	     	<p class="estilo-h3">
 	     	|HUGO CASANOVA</p>
 	  	    <p class="estilo-p">
@@ -12,14 +13,14 @@
          </article>
 	</div>
 	<div class="row justify-content-center">
-		<article class="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+		<article class="col-xs-12 col-sm-6 col-md-4 col-lg-5">
 			<p  class="estilo-h3">
 			|ALEXANDER CORDERO</p>
 			<p class="estilo-p">
-			&nbsp;&nbsp;DIRECTOR COMERCIAL</br>
-		    &nbsp;&nbsp;H CASANOVA REPRESENTACIONES, C.A</P>
+			&nbsp;&nbsp;DIRECTOR COMERCIAL </br>
+			&nbsp;&nbsp;H CASANOVA REPRESENTACIONES, C.A</P>
 		</article>
-		<article  class="col-xs-12 col-sm-5 col-md-6 col-lg-5">
+		<article  class="col-xs-12 col-sm-5 col-md-5 col-lg-5">
 			<p class="estilo-h3">
 			|RODRIGO TRONCOSO</p>
 			<p class="estilo-p">
@@ -28,14 +29,14 @@
 		</article>
 	</div>	
 	<div class="row justify-content-center">
-		<article class="col-xs-12 col-sm-4 col-md-6 col-lg-4">
+		<article class="col-xs-12 col-sm-6 col-md-4 col-lg-5">
 			<p class="estilo-h3">
 			|CESAR MENA</p>
 			<p class="estilo-p">
 			&nbsp;&nbsp;GERENTE GENERAL</br>
 		    &nbsp;&nbsp;CASANOVA INTERNACIONAL, LLC</P>
 		</article>
-		<article  class="col-xs-12 col-sm-5 col-md-6 col-lg-5">
+		<article  class="col-xs-12 col-sm-5 col-md-5 col-lg-5">
 			<p class="estilo-h3">
 			|VIVIANA CASANOVA</p>
 			<p class="estilo-p">

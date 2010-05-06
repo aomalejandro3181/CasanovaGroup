63550fffc7c44f2ba234100800075e3c9faf5d02
winderson <winderson705@gmail.com>
2010-05-06 00:24:43 -0430

    images

---
+++ wp-content/themes/casanova/html/footer.php
@@ -0,0 +1,7 @@
+<div id="footer" class="container-fluid">
+	<div class="row justify-content-center">
+		<div class="col-12 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+			<p></p>
+		</div>
+	</div>
+</div>

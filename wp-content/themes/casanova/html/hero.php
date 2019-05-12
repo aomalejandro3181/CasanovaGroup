d7a59a0ed037a9bb215c5f9576e96a4f656f8177
Alejandro Olivar <aomalejandro@gmail.com>
2019-05-11 22:50:20 -0400

    hero slider

---
+++ wp-content/themes/casanova/html/hero.php
@@ -0,0 +1,22 @@
+<section id="hero" class="container-fluid no-gutters">
+    <div class="hero-sliders">
+        <div class="slider">
+            <img src="https://via.placeholder.com/1600x600.png?text=Slide1" alt="">
+            <div class="container">
+                <div class="row align-items-center">
+                <div class="col-6 cont-title">
+                    <h1 class="title">Somos Soluciones Energéticas para tu mercado</h1>
+                    <h3 class="sub-title">Contamos con un amplio catálogo de productos que se ajustaran a tu necesidad industrial</h3>
+                    <a class="border rounded-pill btn-hero" href="#contactos">Contactanos</a>
+                </div>
+                </div>
+            </div>
+        </div>
+        <div class="slider">
+            <img src="https://via.placeholder.com/1600x600.png?text=Slide2" alt="">
+        </div>
+        <div class="slider">
+        <img src="https://via.placeholder.com/1600x600.png?text=Slide3" alt="">
+        </div>
+    </div>
+</section>
---
+++ wp-content/themes/casanova/html/js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $('.hero-sliders').slick({
+    dots: true,
+    speed: 800,
+    arrows: false
+  });
+});

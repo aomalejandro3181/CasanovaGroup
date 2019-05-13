bafda42233a8f81edb755d49d94295084e757f58
winderson <winderson705@gmail.com>
2019-05-13 17:44:52 -0430

    equipo,contacto

---
+++ wp-content/themes/casanova/html/contacto.php
@@ -0,0 +1,18 @@
+<section class="container-fluid">
+<div class="row justify-content-center">
+	 <div class="col-9">
+	 	<h3 class="text-warning">Contacto</h3>
+	 	<input class="border rounded-pill col-6 border-secondary" type="email" name="correo" placeholder="Correo Electronico"></br>
+	 	<input class="border rounded-pill col-3 border-secondary" type="text" name="Nombre" placeholder="Nombre">
+		<input class="border rounded-pill col-3 border-secondary" type="text" name="Apellido" placeholder="Apellido"></br>
+		<input class="border rounded-pill col-3 border-secondary" type="text" name="Empresa" placeholder="Empresa">
+	    <input class="border rounded-pill col-3 border-secondary" type="number" name="Telefono" placeholder="Telefono"></br>
+	    <input class="border rounded-pill col-6 border-secondary" type="text" name="Asunto" placeholder="Asunto"></br>
+	    <input class="border rounded-pill col-6 border-secondary" type="message" name="Mensaje" placeholder="Mensaje"></br>
+	    <input class="border rounded-pill col-2 bg-white border-primary btn-enviar" type="button" name="Enviar" value="Enviar">
+	 </div>
+		
+		
+</div>
+	
+</section>
---
+++ wp-content/themes/casanova/html/equipo.php
@@ -0,0 +1,46 @@
+<section class="container-fluid bg-secondary">
+	<div class="row justify-content-center ">
+	  <article class="col-9 ">
+	  	<h2 class="text-white">Equipo</h2></BR>
+	   </article>
+	      <article class="w-100"></article>
+	    <article class="col-9">
+	     	<h3 class="text-white">
+	     	|HUGO CASANOVA</h3>
+	  	    <p class="text-warning">
+	  	    &nbsp;&nbsp;CEO</P>
+         </article>
+	</div>
+	<div class="row justify-content-center">
+		<article class="col-4">
+			<h3 class="text-white">
+			|ALEXANDER CORDERO</h3>
+			<p class="text-warning">
+			&nbsp;&nbsp;DIRECTOR COMERCIAL</br>
+		    &nbsp;&nbsp;H CASANOVA REPRESENTACIONES, C.A</P>
+		</article>
+		<article class="col-5">
+			<h3 class="text-white">
+			|RODRIGO TRONCOSO</h3>
+			<p class="text-warning">
+			&nbsp;&nbsp;DIRECTOR DE OPERECIONES</br>
+		    &nbsp;&nbsp;H CASANOVA REPRESENTACIONES, C.A</P>
+		</article>
+	</div>	
+	<div class="row justify-content-center">
+		<article class="col-4">
+			<h3 class="text-white">
+			|CESAR MENA</h3>
+			<p class="text-warning">
+			&nbsp;&nbsp;GERENTE GENERAL</br>
+		    &nbsp;&nbsp;CASANOVA INTERNACIONAL, LLC</P>
+		</article>
+		<article class="col-5">
+			<h3 class="text-white">
+			|VIVIANA CASANOVA</h3>
+			<p class="text-warning">
+			&nbsp;&nbsp;DIRECTORA GENERAL</br>
+		    &nbsp;&nbsp;CASANOVA INTERNACIONAL EUROPA, S.L</P>
+		</article>
+	</div>
+</section>

04b2ff83629aab6374fa4785c41d24203d1c4c1d
Alejandro Olivar <aomalejandro@gmail.com>
2019-07-16 10:30:38 -0400

    agregar footer info

--- wp-content/themes/casanova/footer.php
+++ wp-content/themes/casanova/footer.php
@@ -3,9 +3,37 @@
  * @package WordPress
  * @subpackage HTML5_Boilerplate
  */
+
+$args = array('post_type' => 'settings');
+$loop = new WP_Query($args);
+
+if ($loop->have_posts()) :
+    while ($loop->have_posts()) :
+        $loop->the_post();
+        // vars
+        $section_footer       = get_field('footer');
+    endwhile;
+//end of the loop.
+else :?>
+    <h1>No posts here!</h1>
+<?php endif;
+
+//print_r($section_footer);
 ?>
 
   <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-md-6 col-lg-4">
+          <p class="title"><b><?php echo $section_footer['title']?></b></p>
+          <address class="content"><?php echo $section_footer['direccion']?></address>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <p class="title"><b><?php echo $section_footer['title_2']?></b></p>
+          <address class="content"><?php echo $section_footer['direccion_2']?></address>
+        </div>
+      </div>
+    </div>
   </footer>
 
   <!-- Javascript at the bottom for fast page loading -->
